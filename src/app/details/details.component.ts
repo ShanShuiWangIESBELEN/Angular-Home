8c8acdaf887a7194d960ec40d4cbecf2f4519d23
Wss <wss1259227230@gmail.com>
2025-02-17 08:42:20 +0100

    jsonServer

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,7 +8,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   selector: 'app-details',
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-  <article>
+    <article>
       <img
         class="listing-photo"
         [src]="housingLocation?.photo"
@@ -42,21 +42,21 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     </article>
   `,
   styleUrls: ['./details.component.css'],
-
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
-
   applyForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
   });
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+    });
   }
   submitApplication() {
     this.housingService.submitApplication(
